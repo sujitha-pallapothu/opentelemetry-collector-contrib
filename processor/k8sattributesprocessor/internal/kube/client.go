bec33a217c35064bfbaa93b57808d11a79bb7bfb
sujitha.pallapothu <you@example.com>
2025-05-07 13:50:49 +0000

    testing k8sattr

--- processor/k8sattributesprocessor/internal/kube/client.go
+++ processor/k8sattributesprocessor/internal/kube/client.go
@@ -393,6 +393,26 @@ func (c *WatchClient) GetPod(identifier PodIdentifier) (*Pod, bool) {
 	return nil, false
 }
 
+// GetPodByContainerID retrieves a Pod based on the container ID.
+func (c *WatchClient) GetPodByContainerID(containerID string) (*Pod, bool) {
+	c.logger.Debug("Entered GetPodByContainerID", zap.String("containerID", containerID))
+	c.m.RLock() // Use the correct mutex for thread-safe access to the Pods map
+	defer c.m.RUnlock()
+
+	// Iterate through all pods in the Pods map
+	for _, pod := range c.Pods {
+		// Check the container IDs in the pod's containers
+		for id, _ := range pod.Containers.ByID {
+			if id == containerID {
+				return pod, true
+			}
+		}
+	}
+
+	// If no matching pod is found, return nil and false
+	return nil, false
+}
+
 // GetNamespace takes a namespace and returns the namespace object the namespace is associated with.
 func (c *WatchClient) GetNamespace(namespace string) (*Namespace, bool) {
 	c.m.RLock()
